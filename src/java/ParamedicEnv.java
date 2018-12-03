805cc9e21242b4311516f4652508f61d9ee80c95
benhawley7 <benhawley7@gmail.com>
2018-12-03 15:34:19 +0000

    Changed the doctor code for testing

--- src/java/ParamedicEnv.java
+++ src/java/ParamedicEnv.java
@@ -221,17 +221,28 @@ public class ParamedicEnv extends Environment {
 //                ~critical(4,5).
 //                ~critical(5,1).
                 
-                if (x == 2 && y == 3) {
+//                ~critical(2,0).
+//                ~critical(2,2).
+//                critical(5,4).
+                
+                
+//                critical(2,5).
+//                ~critical(5,5).
+//                critical(3,1).
+                
+//                critical(1,3).
+//                ~critical(4,3).
+//                ~critical(0,2).
+
+                
+                if (x == 1 && y == 3) {
                     Literal location = Literal.parseLiteral("colour(" + x + "," + y + ", burgandy)");
                     addPercept("paramedic", location);
-                } else if (x == 4 && y == 5) {
+                } else if (x == 4 && y == 3) {
                     Literal location = Literal.parseLiteral("colour(" + x + "," + y + ", cyan)");
                     addPercept("paramedic", location);
-                } else if (x == 5 && y == 1) {
+                } else if (x == 0 && y == 2) {
                     Literal location = Literal.parseLiteral("colour(" + x + "," + y + ", cyan)");
-                    addPercept("paramedic", location);
-                } else if (x == 5 && y == 5) {
-                    Literal location = Literal.parseLiteral("colour(" + x + "," + y + ", burgandy)");
                     addPercept("paramedic", location);
                 } else {
                 	Literal location = Literal.parseLiteral("colour(" + x + "," + y + ", white)");
